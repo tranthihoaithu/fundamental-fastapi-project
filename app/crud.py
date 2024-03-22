b6240e1bb48ba6f0f8e9d5eef06a148929b22ffc
tranhoaithu <tranthihoaithu1112002@gmail.com>
2024-03-22 23:44:46 +0700

    feat:create, update, read,delete, list product

--- app/crud.py
+++ app/crud.py
@@ -6,7 +6,7 @@ from .security import hash_password
 # Create
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = hash_password(user.password)
-    db_user = models.User( username=user.username, email=user.email, password=hashed_password)
+    db_user = models.User(username=user.username, email=user.email, password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -17,3 +17,35 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email== email).first()
 
 
+# create product
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(name=product.name, price=product.price, description=product.description, stock_quantity=product.stock_quantity)
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+# update
+def update_product(db: Session, product_id: int, product_data: schemas.ProductUpdate):
+    db_product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if db_product:
+        for key, value in product_data.dict().items():
+            if value is not None:
+                setattr(db_product, key, value)
+        db.commit()
+        db.refresh(db_product)
+    return db_product
+
+
+# read product
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+
+# Delete
+def delete_product(db: Session, product_id: int):
+    db_product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+    return db_product
--- app/main.py
+++ app/main.py
@@ -39,3 +39,31 @@ def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+@app.post("/products/", response_model=schemas.ProductCreate)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    db_product = crud.create_product(db=db, product=product)
+    return db_product
+
+
+@app.put("/products/{product_id}", response_model=schemas.ProductUpdate)
+def update_product(product_id: int, product_data: schemas.ProductUpdate, db: Session = Depends(get_db)):
+    db_product = crud.update_product(db=db, product_id=product_id, product_data=product_data)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
+
+
+@app.get("/products/{product_id}", response_model=schemas.Product)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = crud.get_product(db=db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404 , detail="product not found")
+    return db_product
+
+
+@app.delete("/products/{product_id}", response_model=schemas.Product)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = crud.delete_product(db=db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
